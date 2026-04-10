9952bcc5f52aea7d2b158330b7819dfef5dbe508
Tonmoy <tonmoy@Tonmoys-Mac-mini.local>
2026-04-10 21:40:23 +0600

    finished work

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { FaFacebookSquare } from "react-icons/fa";
+import { TbBrandInstagramFilled } from "react-icons/tb";
+import { FaXTwitter } from "react-icons/fa6";
+
 
 const Footer = () => {
     return (
@@ -7,10 +11,56 @@ const Footer = () => {
                 <div className='grid grid-cols-6'>
                     <div className='col-span-2 space-y-2.5'>
                         <h2 className='text-4xl font-bold'>Digitools</h2>
-                        <p className='opacity-80 '>Premium digital tools for creators, professionals, and businesses. Work smarter with our suite of powerful tools.</p>
+                        <p className='opacity-80 leading-7'>Premium digital tools for creators, professionals, and businesses. Work smarter with our suite of powerful tools.</p>
                     </div>
-                    <div></div>
-                    <div></div>
+                    <div className='space-y-4'>
+                        <h2 className='text-xl font-medium leading-7'>Product</h2>
+                        <ul className='space-y-4'>
+                            <li className='opacity-80'><a href=""> Features</a></li>
+                            <li className='opacity-80'><a href=""> Pricing</a></li>
+                            <li className='opacity-80'><a href=""> Templates</a></li>
+                            <li className='opacity-80'><a href=""> Integrations</a></li>
+                        </ul>
+                    </div>
+
+                    <div className='space-y-4'>
+                        <h2 className='text-xl font-medium leading-7'>Company</h2>
+                        <ul className='space-y-4'>
+                            <li className='opacity-80'><a href=""> About</a></li>
+                            <li className='opacity-80'><a href=""> Blog</a></li>
+                            <li className='opacity-80'><a href=""> Careers</a></li>
+                            <li className='opacity-80'><a href=""> Press</a></li>
+                        </ul>
+                    </div>
+
+                    <div className='space-y-4'>
+                        <h2 className='text-xl font-medium leading-7'>Resources</h2>
+                        <ul className='space-y-4'>
+                            <li className='opacity-80'><a href=""> Documentation</a></li>
+                            <li className='opacity-80'><a href=""> Help Center</a></li>
+                            <li className='opacity-80'><a href=""> Community</a></li>
+                            <li className='opacity-80'><a href=""> Contact</a></li>
+                        </ul>
+                    </div>
+
+                    <div className='space-y-4'>
+                        <h2 className='text-xl font-medium leading-7'>Social Links</h2>
+                       <ul className='flex items-center gap-4'>
+  <li className=' p-2 rounded-full bg-[#ffffff] flex items-center justify-center'>
+    <a className='text-black'  href=""><TbBrandInstagramFilled size={30} /></a>
+  </li>
+
+  <li className=' p-3 rounded-full bg-white flex items-center justify-center'>
+    <a className='text-black'  href=""><FaFacebookSquare size={20} /></a>
+  </li>
+
+  <li className=' p-3 rounded-full bg-white flex items-center justify-center'>
+    <a className='text-black' href=""><FaXTwitter size={20} /></a>
+  </li>
+</ul>
+                    </div>
+
+                    
                     <div></div>
                 </div>
             </div>
--- src/Components/Transform/Transform.jsx
+++ src/Components/Transform/Transform.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Transform = () => {
     return (
-        <div className='manrope-font mx-auto container py-30 bg-linear-to-r from-[#4F39F6] via-[#801FF9] to-[#9514FA]'>
-            <div className='flex flex-col items-center justify-center gap-5'>
+        <div className='manrope-font  py-30 bg-linear-to-r from-[#4F39F6] via-[#801FF9] to-[#9514FA]'>
+            <div className='container mx-auto flex flex-col items-center justify-center gap-5'>
                 <h1 className='text-[40px] text-white font-bold'>Ready to Transform Your Workflow?</h1>
                 <p className='text-white text-center opacity-90'>Join thousands of professionals who are already using Digitools to work smarter. <br />Start your free trial today.</p>
                 <div className='flex gap-4 mt-5'>
